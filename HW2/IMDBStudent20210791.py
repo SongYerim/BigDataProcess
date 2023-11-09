b4fa6c38636da65fe6bee0bda9babf5036b7bae3
SongYerim <102975547+SongYerim@users.noreply.github.com>
2023-11-09 19:13:43 +0900

    Update IMDBStudent20210791.py

--- HW2/IMDBStudent20210791.py
+++ HW2/IMDBStudent20210791.py
@@ -2,46 +2,24 @@
 import sys
 
 openf = sys.argv[1]
-f = open(openf, "rt")
-animation=0;children=0;comedy=0;adventure=0;fantasy=0;romance=0;drama=0;action=0;crime=0;thriller=0;horror=0;sci=0;doc=0
-for line in f:
-  line = line.strip()
-  text = line.split("::")
-  genre = text[2]
-  genre = genre.split("|")
-  for i in genre:
-    if i == "Animation":
-      animation += 1
-    elif i == "Children's":
-      children += 1
-    elif i == "Comedy":
-      comedy += 1
-    elif i == "Adventure":
-      adventure += 1
-    elif i == "Fantasy":
-      fantasy += 1
-    elif i == "Romance":
-      romance += 1
-    elif i == "Drama":
-      drama += 1
-    elif i == "Action":
-      action += 1
-    elif i == "Crime":
-      crime += 1
-    elif i == "Thriller":
-      thriller += 1
-    elif i == "Horror":
-      horror += 1
-    elif i == "Sci-Fi":
-      sci += 1
-    elif i == "Documentary":
-      doc += 1
-g1 = ["Animation", "Children's", "Comedy", "Adventure", "Fantasy", "Romance", "Drama", "Action", "Crime", "Thriller", "Horror", "Sci-Fi", "Documentary"]
-g2 = [animation, children, comedy, adventure, fantasy, romance, drama, action, crime, thriller, horror, sci, doc]
-countgenre = zip(g1,g2)
+genre_d = dict()
+with open(openf, "rt") as f:
+  for line in f:
+    line = line.strip()
+    text = line.split("::")
+    genre = text[2]
+    genre = genre.split("|")
+    for i in genre:
+      if i in genre_d:
+        genre_d[i] = 1
+      else:
+        genre_d[i] += 1
+name = genre_d.keys()
+count - genre_d.values()
+z = zip(name, count)
+
 closef=sys.argv[2] 
-f = open(closef, "wt")
-for line1,line2 in countgenre:
-  f.write("%s %d\n" %(line1,line2))
-f.close()
+with open(closef, "wt") as f:
+  for key,value in z:
+    f.write(key+" "+str(value)"\n")
     
