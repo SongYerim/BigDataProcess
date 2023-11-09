b0b5659a75b5e7c062963c8dd143fafadc508175
SongYerim <102975547+SongYerim@users.noreply.github.com>
2023-11-09 21:20:01 +0900

    Update UBERStudent20210791.py

--- HW2/UBERStudent20210791.py
+++ HW2/UBERStudent20210791.py
@@ -1,37 +1,23 @@
 #!/usr/bin/python3
 
 import sys
+from datetime from datetime
 wday["MON", "TUE", "WED", "THU", "FRI", "SAT", "SUN"]
-regins=[];eday=[];vehicles=[];trips=[]
+uber_d = dict()
 openf=sys.argv[1]
-f = open(openf, "rt")
-for line in f:
-  line = line.strip()
-  text = line.split(",")
-  day = text[1]
-  day = day.split("/")
-  day = day[1]
-  if day == "1" or day == "8" or day == "15":
-    eday.append(wday[3])
-  elif day == "2" or day == "9":
-    eday.append(wday[4])
-  elif day == "3" or day == "10":
-    eday.append(wday[5])
-  elif day == "4" or day == "11":
-    eday.append(wday[6])
-  elif day == "5" or day == "12":
-    eday.append(wday[0])
-  elif day == "6" or day == "13":
-    eday.append(wday[1])
-  else:
-    eday.append(wday[2])
-  regins.append(text[0])
-  vehicles.append(text[2])
-  trips.append(text[3])
-z = sorted(zip(regins,eday,vehicles,trips))
-#z = sorted(z1, key=lambda x : (x[0], x[1]))
+with open(openf, "rt") as f:
+  for line in f:
+    line = line.strip()
+    text = line.split(",")
+    date = text[1]
+    datetime_date = datetime.strptime(date, '%m/%d/%Y')
+    week = wday[datetime_date.weekday()]  
+    regins=text[0]+','+week+" "+text[2]
+    trips=text[3]
+    if regins not in uber_d:
+      uber_d[regins]=trips
+      
 closef=sys.argv[2]
-f=open(closef, "wt")
-for i,j,k,l in z:
-  f.write("%s,%s %s,%s\n" %(i,j,k,l))
-f.close()
+with open(closef, "wt") as f:
+  for key,value in uber_d.itmes():
+    f.write("%s,%s\n" %(key,value))
